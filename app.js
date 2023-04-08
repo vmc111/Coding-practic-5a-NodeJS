9bfefcf116aa36452615a3f602ad7fcdf2353db5
vmc111 <vamship625@gmail.com>
2023-04-08 13:05:47 +0000

    Passed All TestCAses

--- app.js
+++ app.js
@@ -57,4 +57,89 @@ app.post("/movies/", async (request, response) => {
   response.send("Movie Successfully Added");
 });
 
+// Get with movieId
+
+app.get("/movies/:movieId/", async (request, response) => {
+  const { movieId } = request.params;
+  //   console.log(movieId);
+
+  const getMovieQuery = `
+    Select * 
+    FROM movie
+    WHERE 
+        movie_id = ${movieId};`;
+
+  const requiredPlayer = await db.get(getMovieQuery);
+  const camelCase = (obj) => ({
+    movieId: obj.movie_id,
+    directorId: obj.director_id,
+    movieName: obj.movie_name,
+    leadActor: obj.lead_actor,
+  });
+  response.send(camelCase(requiredPlayer));
+});
+
+// PUT METHOD
+
+app.put("/movies/:movieId/", async (request, response) => {
+  const { movieId } = request.params;
+  const { directorId, movieName, leadActor } = request.body;
+
+  const updateQuery = `
+  UPDATE movie 
+  SET 
+    director_id = ${directorId}, 
+    movie_name = '${movieName}', 
+    lead_actor = '${leadActor}' 
+    WHERE 
+        movie_id = ${movieId};`;
+
+  await db.run(updateQuery);
+  response.send("Movie Details Updated");
+});
+
+// DELETE METHOD
+
+app.delete("/movies/:movieId/", async (request, response) => {
+  const { movieId } = request.params;
+
+  const DeleteQuery = `
+    DELETE 
+        FROM movie 
+    WHERE 
+        movie_id = ${movieId};`;
+
+  await db.run(DeleteQuery);
+  response.send("Movie Removed");
+});
+
+// GET MOVIES BY DIRECTOR_ID
+
+app.get("/directors/:directorId/movies/", async (request, response) => {
+  const { directorId } = request.params;
+
+  const directorQuery = `
+    SELECT 
+        movie_name AS movieName
+    FROM movie 
+    WHERE 
+        director_id = ${directorId}`;
+
+  let movies = await db.all(directorQuery);
+  response.send(movies);
+});
+
+// Get Directors names
+
+app.get("/directors/", async (request, response) => {
+  const directorDetailsQuery = `
+    SELECT 
+    director_id AS directorId, 
+    director_name AS directorName
+    FROM director;`;
+
+  let directorsList = await db.all(directorDetailsQuery);
+  response.send(directorsList);
+});
+
 module.exports = app;
